f1c3db1b3323091c34eb122a3095885243c44591
gouravrathor123 <mayankgouravcool1@gmail.com>
2021-05-07 02:24:58 +0530

    reveiving msg from frontend done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { copyFileSync } = require("fs");
 var http = require("http");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,11 +8,17 @@ var io = require("socket.io")(server);
 
 //middleware
 app.use(express.json());
+var clients = {};
 
 io.on("connection",(socket)=>{
     console.log("Connection is on Bitch!");
     console.log(socket.id, " has joined");
-    socket.on("/test",(msg)=>{
+    socket.on("signin",(id)=>{
+        console.log(id);
+        clients[id]=socket;
+        console.log(clients);
+    });
+    socket.on("message",(msg)=>{
         console.log(msg);
     })
 });
